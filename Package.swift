18ec8691c763deb57d5edb5ba183638f493d1d43
Daemonomead <Daemonomead@users.noreply.github.com>
2022-12-30 10:22:12 +0000

    ClashKit Release 1.0.0!!!

--- Package.swift
+++ Package.swift
@@ -14,3 +14,18 @@ let package = Package(
     )
   ]
 )
+import PackageDescription
+
+let package = Package(
+  name: "ClashKit",
+  products: [
+    .library(name: "ClashKit", targets: ["ClashKit"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "ClashKit",
+      url: "https://github.com/Daemonomead/ClashKit/releases/download/1.0.0/ClashKit.xcframework.zip",
+      checksum: "ca4ad5a70734d4f9a56b5fa43ed1d61b130afc1e985263e1e4f55adcd2f4d7d3"
+    )
+  ]
+)
