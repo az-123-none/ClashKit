b1d0ba2772994635efc1f3b4fe052f47f8349c89
daemonomead <daemonomead@users.noreply.github.com>
2023-01-12 08:30:43 +0000

    ClashKit Release 1.3.10!!!

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "ClashKit",
-      url: "https://github.com/daemonomead/ClashKit/releases/download/1.3.9/ClashKit.xcframework.zip",
-      checksum: "ecc96ac65422e16690bfff82ef685969643f4e2bc70b80bac951e04ec16fbadb"
+      url: "https://github.com/daemonomead/ClashKit/releases/download/1.3.10/ClashKit.xcframework.zip",
+      checksum: "4e41106f86028fae98db1f8e14bc29649da6c34e9d9b0e599ba4210edb22a4c1"
     )
   ]
 )
